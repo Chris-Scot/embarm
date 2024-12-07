5423a261c2c0f6b404650c9622664a632a784cec
Chris <github@chris.scot>
2024-12-07 17:13:26 +0000

    First Load - Updated 07 Dec 17:13

--- Files/AutoMount.sh
+++ Files/AutoMount.sh
@@ -1,14 +1,36 @@
 IFS=$'\n'
-for Each in $(blkid -s TYPE | grep -v '^/dev/loop' | grep -v ' TYPE="squashfs"$'); do
-   ThisType=${Each##*TYPE=}
+for Each in $(blkid -s TYPE -s LABEL | grep -v '^/dev/loop' | grep -v ' TYPE="squashfs" '); do
+   ThisLabel=${Each##*LABEL=\"}
+   ThisLabel=${ThisLabel%%\"*}
    ThisDev=${Each%%:*}
-   Each=${ThisDev##*/}
-   if [ "$Each" != "" ]; then
-      if [ ! -d /media/$Each ]; then
-         mkdir -m0 /media/$Each
+   MountPoint="$ThisLabel"
+   if [ "$MountPoint" = "" ]; then
+      MountPoint=${ThisDev##*/}
+   else
+      if [ ! -d "/media/$MountPoint" ]; then
+         mkdir -m0 "/media/$MountPoint"
       fi
-      if [ "$(stat -c%a "/media/$Each")" = "0" ]; then
-         mount -v $ThisDev /media/$Each
+      if [ "$(stat -c%a "/media/$MountPoint")" = "0" ]; then
+         mount -v $ThisDev "/media/$MountPoint"
+         MountPoint=""
+      elif  mount | grep "$ThisDev on /media/$MountPoint"; then
+         MountPoint=""
+      else
+         MountPoint=${ThisDev##*/}
+      fi
+   fi
+
+   if [ "$MountPoint" != "" ]; then
+      if [ ! -d "/media/$MountPoint" ]; then
+         mkdir -m0 "/media/$MountPoint"
+      fi
+      if [ "$(stat -c%a "/media/$MountPoint")" = "0" ]; then
+         mount -v $ThisDev "/media/$MountPoint"
+         MountPoint=""
+      elif  mount | grep "$ThisDev on /media/$MountPoint"; then
+         MountPoint=""
+      else
+         echo "Don't know how to mount $Each"
       fi
    fi
 done
